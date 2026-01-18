f95124743a81eae5b10245de7b9b9688e7f8e6ef
Artemis Masqui <artemis.masqui@epn.edu.ec>
2026-01-17 19:28:52 -0500

    clases

---
+++ src/main/java/com/arbolesbplus/ArbolesBPlus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.arbolesbplus;
+
+/**
+ *
+ * @author Athenea
+ */
+public class ArbolesBPlus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
